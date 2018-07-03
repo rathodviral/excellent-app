7110e19b6067083d459a15fcdc0c201ef70ba726
Viral Rathod <rvdrathod.viral1989@gmail.com>
2018-07-03 21:13:11 +0530

    Filter change

--- src/app/services/filter.pipe.ts
+++ src/app/services/filter.pipe.ts
@@ -15,11 +15,15 @@ export class FilterPipe implements PipeTransform {
       return i.filter(item => item.filterValueName.toLowerCase().indexOf(f.name.toLowerCase()) !== -1);
     }
 
-    if (!Utilities.isEmptyObj(f.campaignName)) {
-      // return i.filter(item => item.filterValueName.toLowerCase().indexOf(f.name.toLowerCase()) !== -1);
+    if (!Utilities.isEmptyObj(f.optionName)) {
+      return i.filter(item => item.optionName.toLowerCase().indexOf(f.optionName.toLowerCase()) !== -1);
     }
 
-    return i
+    if (!Utilities.isEmptyObj(f.zoneName)) {
+      return i.filter(item => item.zoneName.toLowerCase().indexOf(f.zoneName.toLowerCase()) !== -1);
+    }
+
+    return i;
     // filter items array, items which match and return true will be
     // kept, false will be filtered out
   }
