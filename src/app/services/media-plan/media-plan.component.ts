8b85f71eda246d7c70b63a4e490a3d4b3c505bf1
Viral Rathod <rvdrathod.viral1989@gmail.com>
2018-09-21 20:10:32 +0530

    SEO Archi 2

--- src/app/services/media-plan/media-plan.component.ts
+++ src/app/services/media-plan/media-plan.component.ts
@@ -3,6 +3,8 @@ import { ServicesService } from '../services.service';
 import { CommonService } from '../../shared/services/common.service';
 import { LocalStorage } from '../../shared/constant/local-storage';
 import { Utilities } from '../../shared/services/utilities';
+import { LoginPopupComponent } from '../../shared/components/login-popup/login-popup.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-media-plan',
@@ -12,11 +14,16 @@ import { Utilities } from '../../shared/services/utilities';
 export class MediaPlanComponent implements OnInit {
 
   carts: any[];
+  isCartDisplay: boolean;
+  isLoginPopupDisplay: boolean;
+  userData: any = {};
 
-  constructor(private commonService: CommonService, private servicesService: ServicesService) { }
+  constructor(private commonService: CommonService, public dialog: MatDialog, private servicesService: ServicesService) { }
 
   ngOnInit() {
     this.carts = this.commonService.getLocalStorageObject(LocalStorage.CartData);
+    this.isCartDisplay = false;
+    this.isLoginPopupDisplay = false;
   }
 
   changeInCarts(isEdit, index) {
@@ -36,7 +43,26 @@ export class MediaPlanComponent implements OnInit {
   }
 
   isTokenAvailable() {
+    this.userData = this.commonService.getDataFromLocalStorageObject(LocalStorage.UserData, 'user');
+
     return !Utilities.isEmptyObj(this.commonService.getDataFromLocalStorageObject(LocalStorage.UserData, 'token'));
+  }
+
+  closeLoginDialog(data) {
+    this.isLoginPopupDisplay = data;
+    if (data) {
+      this.loginPopupManage();
+    }
+  }
+
+  loginPopupManage() {
+    const dialogRef = this.dialog.open(LoginPopupComponent, {
+      width: '100%',
+      data: {}
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
 }
